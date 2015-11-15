57f9dcf7c839676e83cbf4bf84529d6d3cb1d7af
jimtoepel <jim.toepel@gmail.com>
2015-11-14 19:13:48 -0800

    New Working Branch
    Old one deprecated.

---
+++ FUNmediaData.h
@@ -0,0 +1,28 @@
+//
+//  FUNmedia.h
+//  intently
+//
+//  Created by Jim Toepel on 10/18/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface FUNmediaData : NSObject
+
+@property (nonatomic) NSString *title;
+@property (nonatomic) NSString *format; // ecosystem? itunes, ps4, etc?
+@property (nonatomic) NSString *kind;   // movie, game, tv show?
+
+@property (nonatomic) UIImage *thumbImage;
+@property (nonatomic) int rating;
+@property (nonatomic) bool playing;
+@property (nonatomic) float hoursPlayed;
+@property (nonatomic) bool finished;
+
+-(id)initWithTitle:(NSString*)newTitle format:(NSString*)newFormat kind:(NSString*)newKind;
+-(void)recordConsumption:(float)a;
+
+
+@end
---
+++ FUNmediaData.m
@@ -0,0 +1,39 @@
+//
+//  FUNmedia.m
+//  intently
+//
+//  Created by Jim Toepel on 10/18/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import "FUNmediaData.h"
+
+@implementation FUNmediaData
+
+@synthesize title;
+@synthesize format;
+@synthesize kind;
+@synthesize thumbImage;
+@synthesize rating;
+@synthesize playing;
+@synthesize hoursPlayed;
+@synthesize finished;
+
+- (id)initWithTitle:(NSString *)newTitle format:(NSString*)newFormat kind:(NSString*)newKind
+{
+    if ((self =[super init] )){
+        self.title = newTitle;
+        self.format = newFormat;
+        self.kind = newKind;
+
+    }
+    return self;
+}
+
+- (void)recordConsumption:(float)a
+{
+    hoursPlayed = hoursPlayed + a;
+}
+
+
+@end
